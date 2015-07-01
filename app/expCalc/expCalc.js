77506193ce9f87bac67ff6ae11a831aa9abffd63
Xezz <xezzxezz@gmail.com>
2015-07-02 00:06:11 +0200

    Nur eine Range von +-8 anzeigen

--- app/expCalc/expCalc.js
+++ app/expCalc/expCalc.js
@@ -10,6 +10,7 @@ angular.module('myApp.expCalc', ['ngRoute'])
     }])
 
     .controller('ExpCalcCtrl', ['$scope', function ($scope) {
+        $scope.range = 20;
         $scope.playerLevel = 1;
         $scope.monsterLevelBoundary = 3;
         $scope.levelTableData = [];
@@ -19,13 +20,18 @@ angular.module('myApp.expCalc', ['ngRoute'])
         $scope.fullExpMaxLevel = function () {
             return $scope.playerLevel + $scope.monsterLevelBoundary;
         };
-        var calculateExpPenality = function (playerLevel, zoneLevel) {
+        var calculateExpPenalty = function (playerLevel, zoneLevel) {
             var maxMonsterLevel = playerLevel + Math.abs(playerLevel - zoneLevel);
             var result = Math.pow(maxMonsterLevel / (maxMonsterLevel + Math.pow(calcPlayerLevelToEffectiveLevel(playerLevel, zoneLevel), 2.5)), 1.5) * 100;
 
             return result - result % 1;
         };
         $scope.getEffectiveLevel = function () {
+            if ($scope.playerLevel < 1) {
+                $scope.playerLevel = 1;
+            } else if ($scope.playerLevel > 100) {
+                $scope.playerLevel = 100;
+            }
             $scope.monsterLevelBoundary = calcEffectiveLevel($scope.playerLevel);
             $scope.getLevelRanges();
         };
@@ -34,23 +40,56 @@ angular.module('myApp.expCalc', ['ngRoute'])
             return 3 + Math.floor(playerLevel / 16);
         };
         var calcPlayerLevelToEffectiveLevel = function (playerLevel, zoneLevel) {
-            // 1, 1
             var boundary = calcEffectiveLevel(playerLevel);
-            var positveDifference = Math.abs(playerLevel - zoneLevel);
+            var positiveDifference = Math.abs(playerLevel - zoneLevel);
 
-            return Math.max(0, positveDifference - boundary);
+            return Math.max(0, positiveDifference - boundary);
 
         };
 
         $scope.getLevelRanges = function () {
             var result = [];
-            for (var zoneLevel = 1; zoneLevel <= 100; zoneLevel++) {
+            var minDif = $scope.playerLevel - $scope.range;
+            var maxDif = $scope.playerLevel + $scope.range;
+            var min = 1;
+            var max = 100;
+            if (minDif < min) {
+                var onTop = minDif - min;
+                maxDif -= onTop;
+                minDif = min;
+            } else if (maxDif > max) {
+                minDif -= maxDif - max;
+                maxDif = max;
+            }
+            for (var zoneLevel = minDif; zoneLevel <= maxDif; zoneLevel++) {
                 result.push({
                     level: zoneLevel,
-                    expPercentage: calculateExpPenality($scope.playerLevel, zoneLevel)
+                    expPercentage: calculateExpPenalty($scope.playerLevel, zoneLevel)
                 });
             }
             $scope.levelTableData = result;
+        };
+        var calcPlayerShare = function (player) {
+            return Math.pow(player.level + 10, 2.71);
+
+        };
+        var calcPartyShare = function (players) {
+            var result = {
+                totalShare: 0,
+                partyMembers: []
+            };
+            if (players === undefined || players.length === undefined || players.length < 1) {
+                return result;
+            }
+            for (var index = 0; index < players.length; index++) {
+                var thatPlayerShare = calcPlayerShare(players[index]);
+                result.totalShare += thatPlayerShare;
+                result.partyMembers.push({
+                    player: players[index],
+                    expShare: thatPlayerShare
+                });
+            }
+            return result;
         };
         $scope.getLevelRanges();
     }]);
